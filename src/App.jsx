568bbda27f01b25f43af272c408ecfbeae98c736
Ijin George <georgeijin@gmail.com>
2025-03-25 17:22:35 +0530

    margin top

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,8 @@ function App() {
 
   return (
     <>
-      <div className="w-screen h-screen flex items-center justify-center bg-[rgb(51,95,112)]">
-        <div className="bg-[rgb(19,36,42)] p-6 rounded-2xl flex flex-col justify-center items-center w-[90%] sm:w-[80%] md:w-[70%] lg:w-[60%] xl:w-[50%]">
+      <div className="w-screen h-screen flex items-center sm:justify-center bg-[rgb(51,95,112)] flex-col">
+        <div className="bg-[rgb(19,36,42)] p-6 rounded-2xl flex flex-col justify-center items-center w-[90%] sm:w-[80%] md:w-[70%] lg:w-[60%] xl:w-[50%] mt-[300px] sm:mt-0">
           <h1 className="text-2xl md:text-4xl lg:text-6xl pb-5 text-white mb-1.5">ToDo List</h1>
           <InputContainer />
           <Form />
